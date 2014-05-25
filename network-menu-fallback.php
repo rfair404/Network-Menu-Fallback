b11ab18b8ab5c7c0262898bc9d72c4ef5d45d2c7
Russell Fair <rfair404@gmail.com>
2014-05-25 12:04:15 -0400

    add the network menu as the fallback

---
+++ network-menu-fallback.php
@@ -0,0 +1,19 @@
+<?php
+/*
+* Plugin Name: Network Menu Fallback
+* Author: Russell Fair
+* Version: 0.2
+* Description: Uses the parent site's menu as the fallback for non-existant menus
+*/
+
+function rlfs_network_menu($args){
+    global $current_site, $current_blog;
+    switch_to_blog($current_site->blog_id);
+    wp_nav_menu(array('theme_location' =>$args['theme_location'], 'menu_class' => $args['menu_class']));
+    restore_current_blog($current_blog->blog_id);
+}
+
+add_filter('wp_nav_menu_args', 'rlfs_network_menu_args' );
+function rlfs_network_menu_args($args){
+    $args['fallback_cb']  = 'rlfs_network_menu';
+}
