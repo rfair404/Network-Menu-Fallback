c2fd293fe54831dabf8b6a1dad6902da1a44f5f5
Russell Fair <rfair404@gmail.com>
2014-05-25 12:05:46 -0400

    add prefix as mnf

--- network-menu-fallback.php
+++ network-menu-fallback.php
@@ -6,14 +6,14 @@
 * Description: Uses the parent site's menu as the fallback for non-existant menus
 */
 
-function rlfs_network_menu($args){
+function nmf_network_menu($args){
     global $current_site, $current_blog;
     switch_to_blog($current_site->blog_id);
     wp_nav_menu(array('theme_location' =>$args['theme_location'], 'menu_class' => $args['menu_class']));
     restore_current_blog($current_blog->blog_id);
 }
 
-add_filter('wp_nav_menu_args', 'rlfs_network_menu_args' );
-function rlfs_network_menu_args($args){
-    $args['fallback_cb']  = 'rlfs_network_menu';
+add_filter('wp_nav_menu_args', 'nmf_network_menu_args' );
+function nmf_network_menu_args($args){
+    $args['fallback_cb']  = 'nmf_network_menu';
 }
